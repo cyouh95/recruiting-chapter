32ab5ded225970cdee4b79941dbf740a27fc9ae8
Ozan Jaquette <ozanj@ucla.edu>
2020-11-06 15:34:09 -0800

    mostly chicken scratch

--- scripts/network_recruiting_analysis.R
+++ scripts/network_recruiting_analysis.R
@@ -1,7 +1,7 @@
 library(igraph)
 library(tidyverse)
 
-options(max.print=99)
+options(max.print=2000)
 ## -------------
 ## LOAD OBJECTS
 ## -------------
@@ -738,22 +738,94 @@ df_temp %>% select(`100751`,`106397`)
 #bipartite.projection(g_2mode)[["proj2"]] # one mode object, mode = universities
 
 
-#  
-df_temp2 <- as_adjacency_matrix(
+#create adjacency matrix of 1-mode university object
+  # create adacency matrix
+  # convert matrix to data frame. ? necessary?
+  #rm(df_1mode_psi)
+affil_1mode_psi <- as_adjacency_matrix(
   graph = bipartite.projection(g_2mode)[["proj2"]],  # creates one-mode object w/ mode = universities
   sparse = FALSE, 
   attr = 'weight'
-) %>% as.data.frame(
+)
+affil_1mode_psi %>% str()
+
+%>% as.data.frame(
   row.names = NULL, 
   optional = FALSE,
   make.names = TRUE,
   stringsAsFactors = FALSE
 ) 
 
-df_temp2 %>% select(`100751`,`106397`)
-%>% str()
+# create "distance matrix" using stats::dist() function
+  # what is distance matrix?
+    # https://online.stat.psu.edu/stat555/node/86/#:~:text=Clustering%20starts%20by%20computing%20a,is%20distance%20zero%20from%20itself).
+      # Clustering starts by computing a distance between every pair of units that you want to cluster.  A distance matrix will be symmetric (because the distance between x and y is the same as the distance between y and x) and will have zeroes on the diagonal (because every item is distance zero from itself).  The table below is an example of a distance matrix.  Only the lower triangle is shown, because the upper triangle can be filled in by reflection.
+dist_1mode_psi <- dist(
+  x = affil_1mode_psi , # 	a numeric matrix, data frame or "dist" object.
+  method = "euclidean", # the distance measure to be used. This must be one of "euclidean", "maximum", "manhattan", "canberra", "binary" or "minkowski". default = euclidian where Usual distance between the two vectors (2 norm aka L_2), sqrt(sum((x_i - y_i)^2)).
+  diag = TRUE, # FALSE, # logical value indicating whether the diagonal of the distance matrix should be printed by print.dist. default = FALSE
+  upper = TRUE, # logical value indicating whether the upper triangle of the distance matrix should be printed by print.dist.
+  p = 2 # The power of the Minkowski distance. default = 2
+)
+dist_1mode_psi %>% str()
+# conduct hierarchical clustering using stats::hclust
+  # https://www.datacamp.com/community/tutorials/hierarchical-clustering-R
+m <- "complete" # "average" "complete"
+hclust_1mode_psi <- hclust(
+  d = dist_1mode_psi, # a dissimilarity structure as produced by dist() function
+  method = m, # default = complete; the agglomeration method to be used. This should be (an unambiguous abbreviation of) one of "ward.D", "ward.D2", "single", "complete", "average" (= UPGMA), "mcquitty" (= WPGMA), "median" (= WPGMC) or "centroid" (= UPGMC).
+  members = NULL # NULL or a vector with length size of d. See the ‘Details’ section. default = NUKK
+)
+hclust_1mode_psi %>% class()
+hclust_1mode_psi %>% str()
+plot(hclust_1mode_psi)
 
-names(df_temp)
+# cut the dendrogram to create the desired number of clusters using stats::cutree
+  # https://www.datacamp.com/community/tutorials/hierarchical-clustering-R
+  # note: At least one of k or h must be specified, k overrides h if both are given.
+  # choosing number of clusters by specifying k
+  # choosing number of clusters by specifiying height
+    # how is "height" defined
+      # 
+    # from http://ccicada.org/wp-content/uploads/2017/06/Community-Detection-with-Hierarchical-Clustering-Algorithms-Feb-3-2017.pdf 
+    # Note. One way to subdivide a network into clusters is to choose a height along the y-axis of the dendrogram, and draw a horizontal line at that height (see the red line in the above dendrogram), and count how many vertical lines it crosses. Each vertical line corresponds to a grouping of clusters, and the members of that grouping are represented by the branches of the dendrogram that lie below the line
+
+cut_1mode_psi <- cutree(
+  tree = hclust_1mode_psi, # a tree as produced by hclust. cutree() only expects a list with components merge, height, and labels, of appropriate content each.
+  #k = 3, # NULL, # default = NULL an integer scalar or vector with the desired number of groups
+  h = 600 # default = NULL numeric scalar or vector with heights where the tree should be cut
+)
+cut_1mode_psi
+cut_1mode_psi %>% str() # this is just a named atomic vector
+
+df_cut_1mode_psi <- tibble(
+  univ_id = names(cut_1mode_psi),
+  cluster = cut_1mode_psi
+)
+
+g_2mode
+vertex_attr_names(g_2mode)
+V(g_2mode)$name[V(g_2mode)$type == TRUE]
+
+(as.data.frame(V(g_2mode)$name) %>% left_join(y = df_cut_1mode_psi, by = c(`V(g_2mode)$name` = "univ_id")))$cluster %>% str()
+
+vertex_attr(graph = g_2mode, name = "temp") <- (as.data.frame(V(g_2mode)$name) %>% left_join(y = df_cut_1mode_psi, by = c(`V(g_2mode)$name` = "univ_id")))$cluster
+
+vertex_attr(graph = g_2mode, name = "temp")
+vertex_attr(graph = g_2mode, name = "temp") %>% as.numeric()
+
+# a different approach, but this one doesn't work either
+set_vertex_attr(graph = g_2mode, name = "temp", index = V(g_2mode)[V(g_2mode)$type == TRUE], df_cut_1mode_psi$cluster)
+
+vertex_attr(graph = g_2mode, name = "temp")
+vertex_attr(graph = g_2mode, name = "temp") %>% as.numeric()
+
+V(g_2mode) %>% str()
+V(g_2mode)[V(g_2mode)$type == TRUE] %>% str()
+df_cut_1mode_psi %>% str()
+
+
+
 
 # starting w/ the one-mode igraph objects previously created
 
