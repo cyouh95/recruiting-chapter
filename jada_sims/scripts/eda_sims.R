567c080b5365179337cb0157ff6dc546eb2140bc
Ozan Jaquette <ozanj@ucla.edu>
2024-03-04 11:49:37 -0800

    created file structure for jada

---
+++ jada_sims/scripts/eda_sims.R
@@ -0,0 +1,49 @@
+################################################################################
+##
+## [ PROJ ] Recruiting visits by selective private colleges and universities
+## [ FILE ] eda_sims.R
+## [ AUTH ] jadassims4
+## [ INIT ] 3/4/2024
+##
+################################################################################
+
+## ---------------------------
+## libraries
+## ---------------------------
+
+library(igraph)
+library(tidyverse)
+library(labelled)
+
+## ---------------------------
+## directory paths
+## ---------------------------
+
+#rm(list = ls()) # this command deletes all objects from global environment
+
+# data filepath
+#getwd()
+#list.files()
+#list.files(path = 'data')
+#data_dir <- file.path('.','data')
+#data_dir
+#list.files(path = data_dir)
+
+# jada scripts filepath
+list.files(path = 'jada_sims/scripts')
+jada_scripts_dir <- file.path('.','jada_sims','scripts')
+list.files(path = jada_scripts_dir)
+
+# repository scripts filepath
+
+# jada figures filepath
+
+## -----------------------------------------------------------------------------
+## Part X - Label each question using comments
+## -----------------------------------------------------------------------------
+
+
+
+## -----------------------------------------------------------------------------
+## END SCRIPT
+## -----------------------------------------------------------------------------
